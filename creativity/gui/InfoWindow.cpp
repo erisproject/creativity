8408fa37bc724ca18bf178ab5075b7cae3c91693
Jason Rhinelander <jason@imaginary.ca>
2014-11-18 20:14:57 -0500

    Added V matrices; report s2; replaced macros.
    The macros happening here were insane; replaced them with sane methods
    instead.
    
    Added ability to add a set of matrix labels to a grid.  This uses a
    (r+1)*(c+1) set of grid labels; the first row and column is used for
    labels, the remaining is the matrix element labels.
    
    This is now used to display the V matrix for the beliefs.

--- creativity/gui/InfoWindow.cpp
+++ creativity/gui/InfoWindow.cpp
@@ -9,76 +9,91 @@ using namespace creativity::state;
 
 namespace creativity { namespace gui {
 
-#define NEW_GRID(GRID) \
-    grids_.emplace_back(); \
-    Gtk::Grid &GRID = grids_.back(); \
-    GRID.set_row_spacing(5); \
-    GRID.set_column_spacing(5); \
-    GRID.set_row_homogeneous(false); \
-    GRID.set_column_homogeneous(true); \
-    GRID.set_margin_top(10); \
-    GRID.set_margin_left(10); \
-    GRID.set_margin_bottom(10); \
-    GRID.set_margin_right(10); \
-    gpos = 0;
+Gtk::Grid& InfoWindow::new_grid() {
+    grids_.emplace_back();
+    Gtk::Grid &g = grids_.back();
+    g.set_row_spacing(5);
+    g.set_column_spacing(5);
+    g.set_row_homogeneous(false);
+    g.set_column_homogeneous(false);
+    g.set_margin_top(10);
+    g.set_margin_left(10);
+    g.set_margin_bottom(10);
+    g.set_margin_right(10);
+    gpos_ = 0;
+    return g;
+}
 
-#define NEW_TAB_GRID(GRID, NB, TABNAME) \
-    NEW_GRID(GRID); \
-    NB.append_page(GRID, TABNAME);
+Gtk::Grid& InfoWindow::new_tab_grid(Gtk::Notebook &notebook, const std::string &title) {
+    auto &g = new_grid();
+    notebook.append_page(g, title);
+    return g;
+}
+
+void InfoWindow::data_at(Gtk::Grid &grid, const std::string &code, const std::string &value_name, int row, int col) {
+    fields_[code].first.set_markup(value_name + ":");
+    fields_[code].first.set_alignment(1);
+    fields_[code].second.set_alignment(0);
+    grid.attach(fields_[code].first, col, row, 1, 1);
+    grid.attach(fields_[code].second, col+1, row, 1, 1);
+}
+
+void InfoWindow::data_append(Gtk::Grid &grid, const std::string &code, const std::string &value_name) {
+    data_at(grid, code, value_name, gpos_, 0);
+    gpos_++;
+}
+
+void InfoWindow::matrix_at(Gtk::Grid &grid, const std::string &code, const std::string &name, int row, int col, int nrows, int ncols) {
+    // Matrix label:
+    label_at(grid, name, 1.0, row, col, 1, 1);
+
+    // Column labels:
+    for (int j = 1; j <= ncols; j++) label_at(grid, "[," + std::to_string(j) + "]", 1.0, row, col+j, 1, 1);
+
+    auto &labels = matrix_[code];
+    for (int i = 1; i <= nrows; i++) {
+        // Row label:
+        label_at(grid, "[" + std::to_string(i) + ",]", 1.0, row+i, col, 1, 1);
+
+        // Matrix element labels:
+        for (int j = 1; j <= ncols; j++) {
+            labels.emplace_back(new Gtk::Label);
+            Gtk::Label &l = *labels.back();
+            l.set_alignment(0); // FIXME?
+            grid.attach(l, col+j, row+i, 1, 1);
+        }
+    }
+}
 
 
-// Creates a "value: data" row with labels stored in fields_ that can be updated later.  Unlike
-// DATA_ROW, this takes a ROW and COL, and does not use gpos; the two labels are at (ROW,COL)
-// and (ROW,COL+1)
-#define DATA_AT(GRID, CODE, LABEL, ROW, COL) do { \
-    fields_[CODE].first.set_markup(LABEL ":"); \
-    fields_[CODE].first.set_alignment(1); \
-    fields_[CODE].second.set_alignment(0); \
-    GRID.attach(fields_[CODE].first, COL, ROW, 1, 1); \
-    GRID.attach(fields_[CODE].second, COL+1, ROW, 1, 1); \
-    } while (0)
+void InfoWindow::label_at(Gtk::Grid &grid, const std::string &label, double alignment, int row, int col, int width, int height) {
+    labels_.emplace_back();
+    Gtk::Label &a = labels_.back();
+    a.set_alignment(alignment);
+    a.set_markup(label);
+    grid.attach(a, col, row, width, height);
+}
 
-/// Creates a "value: data" row with labels stored in fields_ that can be updated later
-#define DATA_ROW(GRID, CODE, LABEL) do { \
-    DATA_AT(GRID, CODE, LABEL, gpos, 0); \
-    gpos++; \
-    } while (0)
+void InfoWindow::labels_at(Gtk::Grid &grid, const std::string &label1, const std::string &label2, int row, int col) {
+    label_at(grid, label1 + ":", 1.0, row, col, 1, 1);
+    label_at(grid, label2, 0.0, row, col+1, 1, 1);
+}
+void InfoWindow::labels_append(Gtk::Grid &grid, const std::string &label1, const std::string &label2) {
+    labels_at(grid, label1, label2, gpos_++, 0);
+}
+void InfoWindow::comment_append(Gtk::Grid &grid, const std::string &comment, int cols) {
+    labels_.emplace_back();
+    auto &c = labels_.back();
+    c.set_markup(comment);
+    c.set_line_wrap(true);
+    c.set_max_width_chars(80);
+    c.set_margin_top(10);
+    c.set_margin_bottom(10);
+    grid.attach(c, 0, gpos_++, cols, 1);
+}
 
-// Creates a single label at the given location, taking up the given number of rows/columns
-#define LABEL_AT(GRID, LABEL, ROW, COL, WIDTH, HEIGHT, ALIGNMENT) do { \
-    labels_.emplace_back(); Gtk::Label &a = labels_.back(); \
-    a.set_alignment(ALIGNMENT); a.set_markup(LABEL); \
-    GRID.attach(a, COL, ROW, WIDTH, HEIGHT); \
-    } while (0)
 
-// Creates a fixed "value: value" label pair on the given row in columns COL and COL+1.
-#define LABELS_AT(GRID, LABEL, VALUE, ROW, COL) do { \
-    LABEL_AT(GRID, LABEL ":", ROW, COL, 1, 1, 1); \
-    LABEL_AT(GRID, VALUE,   ROW, COL+1, 1, 1, 0); \
-    } while (0)
-
-/// Creates a fixed "value: value" row
-#define LABELS_ROW(GRID, LABEL, VALUE) do { \
-    LABELS_AT(GRID, LABEL, VALUE, gpos, 0); \
-    gpos++; \
-    } while (0)
-
-/// Creates a "value" row (which spans both columns)
-#define COMMENT_ROW(GRID, COMMENT) do { \
-    labels_.emplace_back(); \
-    Gtk::Label &l = labels_.back(); \
-    l.set_markup(COMMENT); \
-    l.set_line_wrap(true); \
-    l.set_max_width_chars(80); \
-    l.set_margin_top(10); \
-    l.set_margin_bottom(10); \
-    GRID.attach(l, 0, gpos++, 2, 1); \
-    } while (0)
-
-#define BETA "<u><i>&#x3b2;</i></u>"
-#define TIMES "&#xd7;"
-#define PI "&#x3c0;"
-#define GTREQ "&#x2265;"
+#define BETA "<i>β</i>"
 
 InfoWindow::InfoWindow(std::shared_ptr<const State> state, std::shared_ptr<Gtk::Window> main_window,
         eris_id_t reader_id, std::function<void(eris::eris_id_t)> open_info_dialog)
@@ -94,77 +109,85 @@ InfoWindow::InfoWindow(std::shared_ptr<const State> state, std::shared_ptr<Gtk::
     nbs_.emplace_back();
     Gtk::Notebook &tabs = nbs_.back();
 
-    int gpos;
-
-    NEW_TAB_GRID(grid_status, tabs, "Status");
-    DATA_ROW(grid_status, "id", "ID");
-    DATA_ROW(grid_status, "position", "Position");
-    DATA_ROW(grid_status, "utility", "Utility");
-    DATA_ROW(grid_status, "uLife", "Lifetime utility");
-    DATA_ROW(grid_status, "books", "Books owned");
-    DATA_ROW(grid_status, "booksPurchased", "Books purchased");
-    DATA_ROW(grid_status, "booksPirated", "Books pirated");
-    DATA_ROW(grid_status, "booksNew", "New books");
-    DATA_ROW(grid_status, "booksNewPurchased", "New books (purchased)");
-    DATA_ROW(grid_status, "booksNewPirated", "New books (pirated)");
-    DATA_ROW(grid_status, "booksWritten", "Books written");
-    DATA_ROW(grid_status, "bookLast", "Latest book age");
-    DATA_ROW(grid_status, "numFriends", "# Friends");
+    auto &grid_status = new_tab_grid(tabs, "Status");
+    data_append(grid_status, "id", "ID");
+    data_append(grid_status, "position", "Position");
+    data_append(grid_status, "utility", "Utility");
+    data_append(grid_status, "uLife", "Lifetime utility");
+    data_append(grid_status, "books", "Books owned");
+    data_append(grid_status, "booksPurchased", "Books purchased");
+    data_append(grid_status, "booksPirated", "Books pirated");
+    data_append(grid_status, "booksNew", "New books");
+    data_append(grid_status, "booksNewPurchased", "New books (purchased)");
+    data_append(grid_status, "booksNewPirated", "New books (pirated)");
+    data_append(grid_status, "booksWritten", "Books written");
+    data_append(grid_status, "bookLast", "Latest book age");
+    data_append(grid_status, "numFriends", "# Friends");
 
     nbs_.emplace_back();
     Gtk::Notebook &beliefs = nbs_.back();
     tabs.append_page(beliefs, "Beliefs");
 
-    NEW_TAB_GRID(grid_quality, beliefs, "Quality");
-    LABELS_ROW(grid_quality, "Dependent variable", "<i>quality</i>");
-    DATA_ROW(grid_quality, "q_n", "n");
-    std::vector<std::string> q_vars{{"constant", "I(firstBook)", "prevBooks", "age", "price", "price" TIMES "age", "copiesSold"}};
+    auto &grid_quality = new_tab_grid(beliefs, "Quality");
+    labels_append(grid_quality, "Dependent variable", "<i>quality</i>");
+    data_append(grid_quality, "q_n", "n");
+    data_append(grid_quality, "q_s2", "s<sup>2</sup>");
+    std::vector<std::string> q_vars{{"constant", "I(firstBook)", "prevBooks", "age", "price", "price×age", "copiesSold"}};
     for (size_t i = 0; i < q_vars.size(); i++)
-        DATA_ROW(grid_quality, "q_" + std::to_string(i), BETA "[" + q_vars[i] + "]");
+        data_append(grid_quality, "q_" + std::to_string(i), BETA "[" + q_vars[i] + "]");
+    matrix_at(grid_quality, "q_V", "<b>V</b>", 2, 2, q_vars.size(), q_vars.size());
 
-    NEW_TAB_GRID(grid_profit, beliefs, "Profit");
-    LABELS_ROW(grid_profit, "Dependent variable", "<i>lifetimeProfit</i>");
-    DATA_ROW(grid_profit, "p_n", "n");
+    auto &grid_profit = new_tab_grid(beliefs, "Profit");
+    labels_append(grid_profit, "Dependent variable", "<i>lifetimeProfit</i>");
+    data_append(grid_profit, "p_n", "n");
+    data_append(grid_profit, "p_s2", "s<sup>2</sup>");
     std::vector<std::string> p_vars{{"constant", "quality", "quality<sup>2</sup>", "I(firstBook)", "previousBooks", "marketBooks"}};
     for (size_t i = 0; i < p_vars.size(); i++)
-        DATA_ROW(grid_profit, "p_" + std::to_string(i), BETA "[" + p_vars[i] + "]");
-    DATA_ROW(grid_profit, "_p_draws", "# successful draws");
-    DATA_ROW(grid_profit, "_p_discards", "# discarded draws");
+        data_append(grid_profit, "p_" + std::to_string(i), BETA "[" + p_vars[i] + "]");
+    data_append(grid_profit, "_p_draws", "# successful draws");
+    data_append(grid_profit, "_p_discards", "# discarded draws");
+    matrix_at(grid_profit, "p_V", "<b>V</b>", 2, 2, p_vars.size(), p_vars.size());
 
-    NEW_TAB_GRID(grid_demand, beliefs, "Demand");
-    COMMENT_ROW(grid_demand, "Note: this regression is for single-period demand");
-    LABELS_ROW(grid_demand, "Dependent variable", "<i>quantityDemanded</i>");
-    DATA_ROW(grid_demand, "d_n", "n");
+    auto &grid_demand = new_tab_grid(beliefs, "Demand");
+    labels_append(grid_demand, "Dependent variable", "<i>quantityDemanded</i>");
+    data_append(grid_demand, "d_n", "n");
+    data_append(grid_demand, "d_s2", "s<sup>2</sup>");
     std::vector<std::string> d_vars{{"constant", "price", "price<sup>2</sup>", "quality", "quality<sup>2</sup>", "prevSales", "age", "I(onlyBook)", "otherBooks", "marketBooks"}};
     for (size_t i = 0; i < d_vars.size(); i++)
-        DATA_ROW(grid_demand, "d_" + std::to_string(i), BETA "[" + d_vars[i] + "]");
-    DATA_ROW(grid_demand, "_d_draws", "# successful draws");
-    DATA_ROW(grid_demand, "_d_discards", "# discarded draws");
+        data_append(grid_demand, "d_" + std::to_string(i), BETA "[" + d_vars[i] + "]");
+    data_append(grid_demand, "_d_draws", "# successful draws");
+    data_append(grid_demand, "_d_discards", "# discarded draws");
+    matrix_at(grid_demand, "d_V", "<b>V</b>", 2, 2, p_vars.size(), p_vars.size());
+    comment_append(grid_demand, "<i>NB: This regression is for single-period demand.</i>", p_vars.size() + 3);
 
-    NEW_TAB_GRID(grid_pstream, beliefs, "Profit Stream");
+    nbs_.emplace_back();
+    Gtk::Notebook &pstream_tabs = nbs_.back();
+    beliefs.append_page(pstream_tabs, "Profit Stream");
 
-    LABEL_AT(grid_pstream, "Dependent variable: <i>profitRemaining</i>", 0, 0, 2*Reader::profit_stream_ages.size(), 1, 0.5);
 
-    int col = 0;
     for (unsigned long a : Reader::profit_stream_ages) {
-        std::string head_label = "<u>age " GTREQ " " + std::to_string(a) + "</u>";
+        auto &grid_pstream = new_tab_grid(pstream_tabs, "age ≥ " + std::to_string(a));
+        labels_append(grid_pstream, "Dependent variable", "<i>profitRemaining</i>");
         std::string code_prefix = "ps" + std::to_string(a) + "_";
-        LABEL_AT(grid_pstream, head_label, 1, col, 2, 1, 0.5);
 
-        DATA_AT(grid_pstream, code_prefix + "n", "n", 2, col);
+        data_append(grid_pstream, code_prefix + "n", "n");
+        data_append(grid_pstream, code_prefix + "s2", "s<sup>2</sup>");
         for (unsigned long j = 0; j < a; j++) {
-            DATA_AT(grid_pstream, code_prefix + std::to_string(j), BETA "[" PI "<sub>" + std::to_string(j) + "</sub>]", 3+j, col);
+            data_append(grid_pstream, code_prefix + std::to_string(j), BETA "[π<sub>" + std::to_string(j) + "</sub>]");
         }
-        col += 2;
+        matrix_at(grid_pstream, code_prefix + "V", "<b>V</b>", 2, 2, a, a);
     }
 
-    NEW_TAB_GRID(grid_pextrap, beliefs, "Profit (extrap.)");
-    LABELS_ROW(grid_pextrap, "Dependent variable", "<i>lifetimeProfit</i>");
-    DATA_ROW(grid_pextrap, "pe_n", "n");
+    auto &grid_pextrap = new_tab_grid(beliefs, "Profit (extrap.)");
+    labels_append(grid_pextrap, "Dependent variable", "<i>lifetimeProfit</i>");
+    data_append(grid_pextrap, "pe_n", "n");
+    data_append(grid_pextrap, "pe_s2", "s<sup>2</sup>");
     for (size_t i = 0; i < p_vars.size(); i++)
-        DATA_ROW(grid_pextrap, "pe_" + std::to_string(i), BETA "[" + p_vars[i] + "]");
-    COMMENT_ROW(grid_pextrap, "<i>NB: This is the same model as the Profit belief, but its data also includes extrapolated values for "
-            "still-on-market books using ProfitStream beliefs, while Profit beliefs only include books once they leave the market.</i>");
+        data_append(grid_pextrap, "pe_" + std::to_string(i), BETA "[" + p_vars[i] + "]");
+    matrix_at(grid_pextrap, "pe_V", "<b>V</b>", 2, 2, p_vars.size(), p_vars.size());
+    comment_append(grid_pextrap, "<i>NB: This is the same model as the Profit belief, but its data also includes extrapolated values for "
+            "still-on-market books using ProfitStream beliefs, while Profit beliefs only include books once they leave the market.</i>",
+            p_vars.size() + 3);
 
     bk_model_ = BookStore::create(state, reader);
     bk_tree_.set_model(bk_model_);
@@ -193,25 +216,23 @@ InfoWindow::InfoWindow(std::shared_ptr<const State> state, std::shared_ptr<Gtk::
 
     set_title("Book details (" + std::to_string(book) + ")");
 
-    int gpos;
+    auto &grid_status = new_grid();
 
-    NEW_GRID(grid_status);
-
-    DATA_ROW(grid_status, "id", "ID");
-    DATA_ROW(grid_status, "position", "Position");
-    DATA_ROW(grid_status, "market", "Market");
-    DATA_ROW(grid_status, "price", "Price");
-    DATA_ROW(grid_status, "quality", "Quality");
-    DATA_ROW(grid_status, "created", "Period written");
-    DATA_ROW(grid_status, "age", "Age");
-    DATA_ROW(grid_status, "revenue", "Revenue (lifetime)");
-    DATA_ROW(grid_status, "revenueLast", "Revenue (current)");
-    DATA_ROW(grid_status, "sales", "Copies sold (lifetime)");
-    DATA_ROW(grid_status, "salesLast", "Copies sold (current)");
-    DATA_ROW(grid_status, "pirated", "Copies pirated (lifetime)");
-    DATA_ROW(grid_status, "piratedLast", "Copies pirated (current)");
-    DATA_ROW(grid_status, "copies", "Copies in world");
-    DATA_ROW(grid_status, "author", "Author ID");
+    data_append(grid_status, "id", "ID");
+    data_append(grid_status, "position", "Position");
+    data_append(grid_status, "market", "Market");
+    data_append(grid_status, "price", "Price");
+    data_append(grid_status, "quality", "Quality");
+    data_append(grid_status, "created", "Period written");
+    data_append(grid_status, "age", "Age");
+    data_append(grid_status, "revenue", "Revenue (lifetime)");
+    data_append(grid_status, "revenueLast", "Revenue (current)");
+    data_append(grid_status, "sales", "Copies sold (lifetime)");
+    data_append(grid_status, "salesLast", "Copies sold (current)");
+    data_append(grid_status, "pirated", "Copies pirated (lifetime)");
+    data_append(grid_status, "piratedLast", "Copies pirated (current)");
+    data_append(grid_status, "copies", "Copies in world");
+    data_append(grid_status, "author", "Author ID");
     override_background_color(Gdk::RGBA{"white"});
 
     add(grid_status);
@@ -255,6 +276,8 @@ void InfoWindow::refresh(std::shared_ptr<const State> state) {
 
 #define UPDATE_LIN(PREFIX, VAR) \
         updateValue(PREFIX + std::string("n"), VAR.n()); \
+        updateValue(PREFIX + std::string("s2"), VAR.s2()); \
+        updateMatrix(PREFIX + std::string("V"), VAR.V()); \
         for (size_t i = 0; i < VAR.K(); i++) \
             updateValue(PREFIX + std::to_string(i), VAR.beta()[i]);
 #define UPDATE_LIN_RB(PREFIX, BELIEF) UPDATE_LIN(PREFIX, r.BELIEF)
@@ -341,6 +364,15 @@ void InfoWindow::updateValue(const std::string &code, double val) {
     updateValue(code, std::to_string(val));
 }
 
+void InfoWindow::updateMatrix(const std::string &code, const Ref<const MatrixXd> &m) {
+    size_t pos = 0;
+    auto &labels = matrix_[code];
+    for (int i = 0; i < m.rows(); i++) { for (int j = 0; j < m.cols(); j++) {
+        if (pos >= labels.size()) return;
+        labels[pos]->set_markup(std::to_string(m(i,j)));
+        pos++;
+    }}
+}
 
 
 } }
--- creativity/gui/InfoWindow.hpp
+++ creativity/gui/InfoWindow.hpp
@@ -14,6 +14,8 @@
 #include "creativity/gui/BookStore.hpp"
 #include "creativity/state/State.hpp"
 
+using namespace Eigen;
+
 namespace creativity {
 
 // forward declarations
@@ -75,13 +77,48 @@ class InfoWindow : public Gtk::Window {
         void updateValue(const std::string &code, long val);
         /// Updates a single value Gtk::Label text with the given double
         void updateValue(const std::string &code, double val);
+        /// Updates a matrix set up with matrix_at with the values of the given matrix.
+        void updateMatrix(const std::string &code, const Ref<const MatrixXd> &m);
 
     private:
         std::unordered_map<std::string, std::pair<Gtk::Label, Gtk::Label>> fields_;
+        std::unordered_map<std::string, std::vector<std::unique_ptr<Gtk::Label>>> matrix_;
         std::list<Gtk::Grid> grids_;
         std::list<Gtk::Label> labels_;
         std::list<Gtk::Notebook> nbs_;
         std::list<Gtk::ScrolledWindow> swins_;
+
+        // The global label position, used during setup.
+        int gpos_ = 0;
+
+        // Initialization helper functions:
+
+        // Create a new grid, store it, return a reference to it (but don't add to any element)
+        Gtk::Grid& new_grid();
+        // Create a new grid and adds it as the content of new tab "title" on `notebook`, returns a
+        // reference to it.
+        Gtk::Grid& new_tab_grid(Gtk::Notebook &notebook, const std::string &title);
+        // Creates a "value: data" label pair with labels stored in fields_ that can be updated
+        // later.  The pair of labels is located at (`row`,`col`) and (`row`,`col+1`).
+        void data_at(Gtk::Grid &grid, const std::string &code, const std::string &value_name, int row, int col);
+        // Calls data_at to put the label at (`gpos_`, 0), then increments gpos_.
+        void data_append(Gtk::Grid &grid, const std::string &code, const std::string &value_name);
+        // Creates a label at the given location, taking up the given number of rows and columns,
+        // with horizontal alignment as given (0-1).  Intended for static values.
+        void label_at(Gtk::Grid &grid, const std::string &label, double alignment, int row, int col, int width, int height);
+        // Creates a pair of single-cell labels at the given (row,col) and (row,col+1).  The first
+        // is right-aligned, the second is left-aligned.
+        void labels_at(Gtk::Grid &grid, const std::string &label1, const std::string &label2, int row, int col);
+        // Creates a pair of static labels at the current (gpos_) row, first two columns.
+        void labels_append(Gtk::Grid &grid, const std::string &label1, const std::string &label2);
+        // Creates a static, wrapping, multi-column label, at the current (gpos_) row.
+        void comment_append(Gtk::Grid &grid, const std::string &comment, int cols = 2);
+        // Creates labels for a matrix of the given size in the grid with [0,0] element at
+        // (`row+1`,`col+1`), storing the labels in row-major order in matrix_.  updateMatrix() can
+        // later be called to update the label values to a given matrix.
+        // (`row`,`col`) gets the given label, and the remainder of the row and column get position
+        // labels.
+        void matrix_at(Gtk::Grid &grid, const std::string &code, const std::string &name, int row, int col, int nrows, int ncols);
 
         std::function<void(eris::eris_id_t)> open_info_dialog_;
 
