erisproject/creativity

commit e39b3fbb2e2ff653e39f865449fa6a5a24694496

Store creation parameters in ReaderState/Storage
The parameters are needed to be able to calculate the effort the reader
required, which is needed to calculate overall profit.

creativity/state/FileStorage.cpp
@@ -629,6 +629,8 @@ std::pair<eris_id_t, ReaderState> FileStorage::readReader(eris_time_t t) const {
     r.cost_unit = read_dbl();
     r.cost_piracy = read_dbl();
     r.income = read_dbl();
+    r.creation_shape = read_dbl();
+    r.creation_scale = read_dbl();
 
     // Beliefs
     belief_data belief = readBelief();
@@ -760,6 +762,9 @@ void FileStorage::writeReader(const ReaderState &r) {
     write_value(r.cost_unit);
     write_value(r.cost_piracy);
     write_value(r.income);
+    // Creation parameters
+    write_value(r.creation_shape);
+    write_value(r.creation_scale);
 
 #ifdef ERIS_DEBUG
     int64_t debug_length = f_.tellp() - debug_start_location;

creativity/Reader.cpp
@@ -115,47 +115,55 @@ const std::vector<double>& Reader::penaltyPolynomial() const {
 }
 
 double Reader::creationQuality(double effort) const {
+    return creationQuality(creation_shape, creation_scale, effort);
+}
+
+double Reader::creationQuality(double shape, double scale, double effort) {
     if (effort < 0)
         throw std::domain_error("Reader::creationQuality() error: effort cannot be negative");
-    if (creation_shape >= 1)
-        throw std::logic_error("Reader::creationQuality() error: creation_shape must be less than 1");
-    if (creation_scale < 0)
-        throw std::logic_error("Reader::creationQuality() error: creation_scale cannot be negative");
+    if (shape >= 1)
+        throw std::logic_error("Reader::creationQuality() error: shape must be less than 1");
+    if (scale < 0)
+        throw std::logic_error("Reader::creationQuality() error: scale cannot be negative");
 
-    if (creation_scale == 0) return 0.0; // This reader always produces 0 quality
+    if (scale == 0) return 0.0; // This reader always produces 0 quality
 
     if (effort == 0) return 0.0; // All of the functional forms of the effort yield q(0) = 0
 
-    return creation_scale * (
-            creation_shape == 0.0 ? std::log(effort + 1.0) : // Special case for log
-            creation_shape == 0.5 ? 2.0 * (std::sqrt(effort + 1.0) - 1.0) : // Slightly more efficient sqrt calculation
-            (std::pow(effort + 1.0, creation_shape) - 1.0) / creation_shape // Otherwise use the full formula
+    return scale * (
+            shape == 0.0 ? std::log(effort + 1.0) : // Special case for log
+            shape == 0.5 ? 2.0 * (std::sqrt(effort + 1.0) - 1.0) : // Slightly more efficient sqrt calculation
+            (std::pow(effort + 1.0, shape) - 1.0) / shape // Otherwise use the full formula
     );
 }
 
 double Reader::creationEffort(double quality) const {
+    return creationEffort(creation_shape, creation_scale, quality);
+}
+
+double Reader::creationEffort(double shape, double scale, double quality) {
     if (quality < 0)
         throw std::domain_error("Reader::creationEffort() error: quality cannot be negative");
-    if (creation_shape >= 1)
-        throw std::logic_error("Reader::creationEffort() error: creation_shape cannot be negative");
-    if (creation_scale < 0)
-        throw std::logic_error("Reader::creationEffort() error: creation_scale cannot be negative");
+    if (shape >= 1)
+        throw std::logic_error("Reader::creationEffort() error: shape cannot be negative");
+    if (scale < 0)
+        throw std::logic_error("Reader::creationEffort() error: scale cannot be negative");
 
     if (quality == 0) return 0.0; // All functional forms yield q(0) = 0, and so q^-1(0) = 0.
-    else if (creation_scale == 0) {
-        // If creation_scale is 0, but quality is not, the function is not invertible (because any
+    else if (scale == 0) {
+        // If scale is 0, but quality is not, the function is not invertible (because any
         // level of effort yields 0 quality), so return infinity.
         return std::numeric_limits<double>::infinity();
     }
-    else if (creation_scale < 0 and quality >= (creation_scale / -creation_shape)) {
-        // When beta (creation_scale) is below 0, there's an asymptote at alpha/-beta; no effort
+    else if (scale < 0 and quality >= (scale / -shape)) {
+        // When beta (scale) is below 0, there's an asymptote at alpha/-beta; no effort
         // level exists that can produce higher quality than that asymptote, so return infinity.
         return std::numeric_limits<double>::infinity();
     }
 
     return
-        creation_shape == 0.0 ? std::exp(quality / creation_scale) - 1.0 : // The special log case
-        std::pow(quality * creation_shape / creation_scale + 1.0, 1.0/creation_shape) - 1.0; // Non-log case
+        shape == 0.0 ? std::exp(quality / scale) - 1.0 : // The special log case
+        std::pow(quality * shape / scale + 1.0, 1.0/shape) - 1.0; // Non-log case
 }
 
 double Reader::piracyCost() const {

creativity/Reader.hpp
@@ -442,6 +442,28 @@ class Reader : public eris::WrappedPositional<eris::agent::AssetAgent>,
          */
         double creationEffort(double quality) const;
 
+        /** Returns the resulting quality of a reader with the given `scale` and `shape` parameters
+         * who expends effort `effort` to create a book.
+         *
+         * This static version of the method is called by the non-static method.
+         *
+         * \sa creation_shape
+         * \sa creation_scale
+         * \sa creationQuality
+         */
+        static double creationQuality(double shape, double scale, double effort);
+
+        /** Returns the required effort of a reader with the given `scale` and `shape` parameters
+         * to create a book of quality `quality`.
+         *
+         * This static version of the method is used by the non-static method.
+         *
+         * \sa creation_shape
+         * \sa creation_scale
+         * \sa creationEffort
+         */
+        static double creationEffort(double shape, double scale, double effort);
+
         /** Called at the end of a period (from BookMarket::intraFinish) to transfer book revenue
          * earned during a period back to the author.  The author subtracts variable cost (if any)
          * and keeps the remaining revenue.  Note that fixed costs are incurred at the beginning of

creativity/state/FileStorage.hpp
@@ -458,6 +458,8 @@ class FileStorage final : public StorageBackend {
          *     dbl              cost_unit
          *     dbl              cost_piracy
          *     dbl              income
+         *     dbl              creation_shape
+         *     dbl              creation_scale
          *     BELIEF           profit belief
          *     BELIEF           profit extrapolated belief
          *     BELIEF           demand belief

creativity/state/PsqlStorage.cpp
@@ -52,8 +52,8 @@ void PsqlStorage::initialConnection() {
     conn_->prepare("friend_ids", "SELECT friend_eris_id FROM friend WHERE reader = $1");
     conn_->prepare("select_library", "SELECT * FROM library WHERE simulation = $1 AND reader_eris_id = $2 WHERE acquired <= $3");
     conn_->prepare("select_beliefs", "SELECT * FROM belief WHERE reader = $1");
-    conn_->prepare("insert_reader", "INSERT INTO reader (state,eris_id,position,u,u_lifetime,cost_fixed,cost_unit,cost_piracy,income) VALUES "
-                                                       "($1" ",$2"   ",$3"    ",$4,$5"     ",$6"      ",$7"     ",$8"       ",$9)");
+    conn_->prepare("insert_reader", "INSERT INTO reader (state,eris_id,position,u,u_lifetime,cost_fixed,cost_unit,cost_piracy,income,creation_shape,creation_scale) VALUES "
+                                                       "($1" ",$2"   ",$3"    ",$4,$5"     ",$6"      ",$7"     ",$8"       ",$9"  ",$10"         ",$11)");
     conn_->prepare("insert_friend", "INSERT INTO friend (reader,friend_eris_id) VALUES ($1,$2)");
     conn_->prepare("exists_library_book", "SELECT COUNT(*) FROM library WHERE simulation = $1 AND reader_eris_id = $2 AND book_eris_id = $3");
     conn_->prepare("insert_library_book", "INSERT INTO library (simulation,reader_eris_id,book_eris_id,type,acquired,quality) VALUES ($1,$2,$3,$4,$5,$6)");
@@ -280,6 +280,8 @@ std::pair<eris::eris_id_t, ReaderState> PsqlStorage::readReader(const pqxx::tupl
     READER_READDBL(cost_unit);
     READER_READDBL(cost_piracy);
     READER_READDBL(income);
+    READER_READDBL(creation_shape);
+    READER_READDBL(creation_scale);
 #undef READER_READDBL
 
     // Friends:

creativity/state/ReaderState.cpp
@@ -18,6 +18,8 @@ ReaderState::ReaderState(const Reader &r) :
     cost_unit{r.cost_unit},
     cost_piracy{r.cost_piracy},
     income{r.income},
+    creation_shape{r.creation_shape},
+    creation_scale{r.creation_scale},
     profit{r.profitBelief()},
     profit_extrap{r.profitExtrapBelief()},
     demand{r.demandBelief()},

creativity/state/ReaderState.hpp
@@ -80,6 +80,12 @@ class ReaderState final {
         /// Per-period (external) income
         double income;
 
+        /// Creation shape coefficient
+        double creation_shape;
+
+        /// Creation scale coefficient
+        double creation_scale;
+
         // Beliefs, copied out at the time the ReaderState object is created
         belief::Profit profit; ///< Profit beliefs
         belief::Profit profit_extrap; ///< Profit beliefs using extrapolation for on-market books

database.pgsql
@@ -205,7 +205,9 @@ CREATE TABLE reader (
     cost_fixed double precision NOT NULL,
     cost_unit double precision NOT NULL,
     cost_piracy double precision NOT NULL,
-    income double precision NOT NULL
+    income double precision NOT NULL,
+    creation_shape double precision NOT NULL,
+    creation_scale double precision NOT NULL
 );
 
 

commit 189e59ee0c6550f089b0bd1d22c258fa06796898

writeReader() debug: account for creation params
Commit e39b3fbb2e2ff653e39f865449fa6a5a24694496 broke under ERIS_DEBUG
(as it should) because the verification calculations enabled under
debugging mode didn't include the new fields.  Fixed.

creativity/state/FileStorage.cpp
@@ -763,7 +763,7 @@ void FileStorage::writeReader(const ReaderState &r) {
     write_value(r.creation_shape);
     write_value(r.creation_scale);
 
-    FILESTORAGE_DEBUG_WRITE_CHECK(4*(1+2*settings_.dimensions+1+r.friends.size()+2+2+2+2+2+2))
+    FILESTORAGE_DEBUG_WRITE_CHECK(4*(1+2*settings_.dimensions+1+r.friends.size()+2+2+2+2+2+2+2+2))
     // Beliefs
     writeBelief(r.profit);
     writeBelief(r.profit_extrap);